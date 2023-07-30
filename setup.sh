24bf1b904f963024dcfa5ad94f858d27187b5548
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-07-31 00:31:44 +0300

    Update setup.sh (added some vscode features)

--- setup.sh
+++ setup.sh
@@ -20,9 +20,26 @@ echo "[+] System upgraded"
 
 # install tool to add repos to apt
 sudo apt install software-properties-common
+echo "[+] software-properties-common tool installed"
 
 # install vscode (and Microsoft key)
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo add-apt-repository 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'
 sudo apt install code -y
+echo "[+] vscode installed"
+
+# install jq to parse json
+sudo apt install jq -y
+echo "[+] jq installed"
+
+# force vscode to always trust folders 
+echo '{"security.workspace.trust.enabled": false}' | jq > $HOME/.config/Code/User/settings.json
+echo "[+] forced vscode to always trust folders"
+
+# install vscode extensions                                                                                                           <-- TBD
+echo "[~] installing vscode extensions"
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension mgmcdermott.vscode-language-babel
+
