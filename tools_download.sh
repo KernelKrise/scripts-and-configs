9bff9070fb51eb4c36e583eec8034dd2ddab28b6
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-08-30 12:59:18 +0300

    Update and rename privesc_download.sh to tools_download.sh
    Added new tools: p0wny shell, Invoke-Mimikatz, PowerSharpPack, procdump. Added check to 'tree' tool. Use 'find' if 'tree' is not installed

--- tools_download.sh
+++ tools_download.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+
+RED="\033[31m"
 GREEN="\033[32m"
 BLUE="\033[34m"
 RESET="\033[0m"
+
 
 TARGET_FOLDER=~/privesc
 echo -e "TARGET FOLDER: $TARGET_FOLDER"
 rm -rf $TARGET_FOLDER
 mkdir -p $TARGET_FOLDER/{windows,linux}
 
-# tree
-echo -e "${GREEN}Downloading tree...${RESET}"
-sudo apt -qq install tree
 
 # pspy
 PSPY_VERSION=$(curl -s https://github.com/DominicBreuker/pspy/tags | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' | sort -u | tail -n 1)
@@ -22,16 +22,20 @@ wget -q https://github.com/DominicBreuker/pspy/releases/download/$PSPY_VERSION/p
 wget -q https://github.com/DominicBreuker/pspy/releases/download/$PSPY_VERSION/pspy32s -O $TARGET_FOLDER/linux/pspy32s 
 wget -q https://github.com/DominicBreuker/pspy/releases/download/$PSPY_VERSION/pspy64s -O $TARGET_FOLDER/linux/pspy64s 
 
-# lp.sh/wp.bat
+
+# linPEAS/WinPEAS
 echo -e "${GREEN}Downloading linPEAS/winPEAS...${RESET}"
 wget -q https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -O $TARGET_FOLDER/linux/lp.sh
 wget -q https://github.com/carlospolop/PEASS-ng/releases/latest/download/winpeas.bat -O $TARGET_FOLDER/windows/wp.bat
 wget -q https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx64.exe -O $TARGET_FOLDER/windows/wp64.exe
 wget -q https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASx86.exe -O $TARGET_FOLDER/windows/wp32.exe
+wget -q https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-winPEAS.ps1 -O $TARGET_FOLDER/windows/Invoke-winPEAS.ps1
+
 
 # socat
 echo -e "${GREEN}Downloading socat...${RESET}"
 wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat -O $TARGET_FOLDER/linux/socat
+
 
 # chisel
 echo -e "${GREEN}Downloading chisel...${RESET}"
@@ -42,14 +46,17 @@ wget -q https://github.com/jpillora/chisel/releases/download/$CHISEL_VERSION/chi
 gzip -d $TARGET_FOLDER/linux/chisel.gz
 gzip -d $TARGET_FOLDER/windows/chisel.exe.gz
 
+
 # PowerView.ps1 / SharpView.exe
 echo -e "${GREEN}Downloading PowerView/SharpView...${RESET}"
 wget -q https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/dev/Recon/PowerView.ps1 -O $TARGET_FOLDER/windows/PowerView.ps1
 wget -q https://github.com/tevora-threat/SharpView/raw/master/Compiled/SharpView.exe-O $TARGET_FOLDER/windows/SharpView.ps1 
 
+
 # Rubeus.exe
 echo -e "${GREEN}Downloading Rubeus...${RESET}"
 wget -q https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe -O $TARGET_FOLDER/windows/Rubeus.exe
+
 
 # Inveigh.ps1 / Inveigh.exe
 echo -e "${GREEN}Downloading Inveigh...${RESET}"
@@ -60,12 +67,14 @@ wget -q "https://github.com/Kevin-Robertson/Inveigh/releases/download/${INVEIGHE
 unzip $TARGET_FOLDER/windows/Inveigh.zip -d $TARGET_FOLDER/windows/
 rm $TARGET_FOLDER/windows/Inveigh.zip $TARGET_FOLDER/windows/Inveigh.pdb
 
+
 # LaZagne.exe
 echo -e "${GREEN}Downloading LaZagne...${RESET}"
 LAZAGNE_VERSION=$(curl -s https://github.com/AlessandroZ/LaZagne/releases | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
 wget -q https://github.com/AlessandroZ/LaZagne/releases/download/$LAZAGNE_VERSION/LaZagne.exe -O $TARGET_FOLDER/windows/LaZagne.exe
 
-# SharpHound.ps1
+
+# SharpHound
 echo -e "${GREEN}Downloading SharpHound...${RESET}"
 wget -q https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1 -O $TARGET_FOLDER/windows/SharpHound.ps1
 SHARPHOUND_EXE_VERSION=$(curl -s https://github.com/BloodHoundAD/SharpHound/releases | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
@@ -74,16 +83,43 @@ wget -q https://github.com/BloodHoundAD/SharpHound/releases/download/v2.0.0/Shar
 unzip $TARGET_FOLDER/windows/SharpHound.zip SharpHound.exe -d $TARGET_FOLDER/windows/
 rm -rf $TARGET_FOLDER/windows/SharpHound.zip
 
+
 # mimikatz.exe
 echo -e "${GREEN}Downloading mimikatz...${RESET}"
 wget -q https://github.com/ParrotSec/mimikatz/raw/master/x64/mimikatz.exe -O $TARGET_FOLDER/windows/mimikatz.exe
+wget -q https://raw.githubusercontent.com/g4uss47/Invoke-Mimikatz/master/Invoke-Mimikatz.ps1 -O $TARGET_FOLDER/windows/Invoke-Mimikatz.ps1
 
 
 # accesschk.exe
 echo -e "${GREEN}Downloading accesschk...${RESET}"
 wget -q https://live.sysinternals.com/accesschk.exe -O $TARGET_FOLDER/windows/accesschk.exe
 
+
+#p0wny shell
+echo -e "${GREEN}Downloading p0wny shell...${RESET}"
+wget -q https://raw.githubusercontent.com/flozz/p0wny-shell/master/shell.php -O $TARGET_FOLDER/linux/shell.php
+
+
+# PowerSharpPack
+wget -q https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpPack.ps1 -O $TARGET_FOLDER/windows/PowerSharpPack.ps1
+
+
+# ProcDump
+echo -e "${GREEN}Downloading ProcDump...${RESET}"
+wget -q https://download.sysinternals.com/files/Procdump.zip -O $TARGET_FOLDER/windows/Procdump.zip
+unzip $TARGET_FOLDER/windows/Procdump.zip procdump.exe -d $TARGET_FOLDER/windows/
+rm $TARGET_FOLDER/windows/Procdump.zip
+
+# Making files executable
 chmod +x $TARGET_FOLDER/windows/*
 chmod +x $TARGET_FOLDER/linux/*
 
-tree $TARGET_FOLDER
+
+# tree command
+if ! command -v tree &> /dev/null; then
+   echo -e "${RED}The 'tree' command is not installed. Install it with your package manager for a nice output of all downloaded utilities${RESET}"
+   echo -e "${GREEN}Running 'find' command instead...${RESET}"
+   find $TARGET_FOLDER
+else
+  tree $TARGET_FOLDER
+fi
