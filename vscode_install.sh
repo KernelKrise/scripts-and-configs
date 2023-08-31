fcdad5bb05edef46f94822151d20696e928ab9df
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-08-31 19:15:55 +0300

    Update vscode_install.sh
    Fixed vscode install

--- vscode_install.sh
+++ vscode_install.sh
@@ -6,6 +6,11 @@ GREEN="\033[32m"
 BLUE="\033[34m"
 RESET="\033[0m"
 
+# install jq to parse json
+echo -e "${BLUE}[~] Installing jq, software-properties-common${RESET}"
+sudo apt install jq software-properties-common -y
+echo -e "${GREEN}[+] jq, software-properties-common installed${RESET}"
+
 
 # install vscode (and Microsoft key)
 echo -e "${BLUE}[~] Installing vscode${RESET}"
@@ -14,12 +19,6 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo add-apt-repository 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'
 sudo apt install code -y
 echo -e "${GREEN}[+] vscode installed${RESET}"
-
-
-# install jq to parse json
-echo -e "${BLUE}[~] Installing jq, software-properties-common${RESET}"
-sudo apt install jq software-properties-common -y
-echo -e "${GREEN}[+] jq, software-properties-common installed${RESET}"
 
 
 # install vscode extensions                                                                                                                 <-- TBD
@@ -32,6 +31,7 @@ echo -e "${GREEN}[+] vscode extensions installed${RESET}"
 
 # force vscode to always trust folders 
 echo -e "${BLUE}[~] Forcing vscode to always trust folders${RESET}"
+mkdir -p $HOME/.config/Code/User
 echo '{"security.workspace.trust.enabled": false}' | jq > $HOME/.config/Code/User/settings.json
 echo -e "${GREEN}[+] Forced vscode to always trust folders${RESET}"
 
