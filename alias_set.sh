b2d53b0e4f15f83bdec41623c121a728f9c6dedf
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-08-31 19:08:23 +0300

    Update alias_set.sh
    added proxychains alias

--- alias_set.sh
+++ alias_set.sh
@@ -13,4 +13,5 @@ echo -e '\n# Custom config:' >> ~/.zshrc
 echo "alias ll='ls -alh'" >> ~/.zshrc
 echo "alias pyhttp='python3 -m http.server 9000'" >> ~/.zshrc
 echo "alias cme='crackmapexec'" >> ~/.zshrc
-echo -e "${GREEN}[+] Added aliases: ll='ls -alh', pyhttp='python3 -m http.server 9000', cme='crackmapexec'${RESET}"
+echo "alias pch='proxychains -q'" >> ~/.zshrc
+echo -e "${GREEN}[+] Added aliases: ll='ls -alh', pyhttp='python3 -m http.server 9000', cme='crackmapexec', pch='proxychains -q'${RESET}"
