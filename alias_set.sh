a7d91aea28de696d658c0cfef51ab1c5ce6de97e
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-08-31 18:18:37 +0300

    Update alias_set.sh

--- alias_set.sh
+++ alias_set.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
 
+
+RED="\033[31m"
+GREEN="\033[32m"
+BLUE="\033[34m"
+RESET="\033[0m"
+
+
+# Add some aliases
+echo -e "${BLUE}[~] Setting up aliases${RESET}"
+echo -e '\n# Custom config:' >> ~/.zshrc
+echo "alias ll='ls -alh'" >> ~/.zshrc
+echo "alias pyhttp='python3 -m http.server 9000'" >> ~/.zshrc
+echo "alias cme='crackmapexec'" >> ~/.zshrc
+echo "${GREEN}[+] Added aliases: ll='ls -alh', pyhttp='python3 -m http.server 9000', cme='crackmapexec'${RESET}"
