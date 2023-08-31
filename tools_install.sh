cd898cbc71b9cdba6510a7501d215ec30ddace59
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-08-31 21:13:40 +0300

    Update tools_install.sh
    fixed docker

--- tools_install.sh
+++ tools_install.sh
@@ -6,6 +6,11 @@ GREEN="\033[32m"
 BLUE="\033[34m"
 RESET="\033[0m"
 
+# update
+echo -e "${BLUE}[~] Updating apt${RESET}"
+sudo apt -y update
+echo -e "${GREEN}[+] apt updated${RESET}"
+
 
 # bloodhound
 echo -e "${BLUE}[~] Installing bloodhound${RESET}"
@@ -14,14 +19,6 @@ echo -e "${GREEN}[+] Bloodhound installed${RESET}"
 
 # docker 
 echo -e "${BLUE}[~] Installing docker${RESET}"
-sudo apt -y install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt -y update
-sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker.io
 echo -e "${GREEN}[+] Docker installed${RESET}"
+
