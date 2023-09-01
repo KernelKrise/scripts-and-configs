52222d5147cd338a91ac7f36415a261901b2a305
Oleksii <76210733+KernelKrise@users.noreply.github.com>
2023-09-01 17:23:16 +0300

    Update tools_install.sh
    Added: bloodhound-python, kerbrute-python, droopescan, ngrok, ROPgadget, one_gadget, pwn_init.py, oletools, z3-solver

--- tools_install.sh
+++ tools_install.sh
@@ -39,3 +39,53 @@ python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pwntools
 echo -e "export PATH=\$PATH:${HOME}/.local/bin" >> ~/.zshrc
 echo -e "${GREEN}[+] pwntools installed${RESET}"
+
+# bloodhound-python
+echo -e "${BLUE}[~] Installing bloodhound-python${RESET}"
+pip3 install bloodhound
+echo -e "${GREEN}[+] bloodhound-python installed${RESET}"
+
+# kerbrute-python
+echo -e "${BLUE}[~] Installing kerbrute-python${RESET}"
+pip3 install kerbrute
+echo -e "${GREEN}[+] kerbrute-python installed${RESET}"
+
+# droopescan
+echo -e "${BLUE}[~] Installing droopescan${RESET}"
+pip install droopescan
+echo -e "${GREEN}[+] droopescan installed${RESET}"
+
+# ngrok
+echo -e "${BLUE}[~] Installing ngrok${RESET}"
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+echo -e "${GREEN}[+] ngrok installed${RESET}"
+
+# ROPgadget
+echo -e "${BLUE}[~] Installing ROPgadget${RESET}"
+sudo apt install python3-pip
+sudo -H python3 -m pip install ROPgadget
+echo -e "${GREEN}[+] ROPgadget installed${RESET}"
+
+# one_gadget
+echo -e "${BLUE}[~] Installing one_gadget${RESET}"
+sudo gem install one_gadget
+echo -e "${GREEN}[+] one_gadget installed${RESET}"
+
+# pwn_init.py
+echo -e "${BLUE}[~] Installing pwn_init.py${RESET}"
+wget -q https://raw.githubusercontent.com/KernelKrise/pwn_init/main/pwn_init.py
+sudo mv pwn_init.py /usr/bin/pwn_init.py
+sudo chmod +x /usr/bin/pwn_init.py
+echo -e "${GREEN}[+] pwn_init.py installed${RESET}"
+
+# oletools
+echo -e "${BLUE}[~] Installing oletools${RESET}"
+pip3 install pyparsing
+pip3 install mitmproxy
+sudo -H pip3 install -U oletools[full]
+echo -e "${GREEN}[+] oletools installed${RESET}"
+
+# z3-solver
+echo -e "${BLUE}[~] Installing z3-solver${RESET}"
+pip install z3-solver
+echo -e "${GREEN}[+] z3-solver installed${RESET}"
